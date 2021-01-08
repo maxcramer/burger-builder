872f116c3812d06bf11e292f32e358549ddaf6e1
maxcramer <maximiliancramer@yahoo.com>
2021-01-08 12:50:39 +0000

    setting up build controls and adding styling :

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './BuildControl.css';
+
+const buildControl = (props) => (
+    <div className="BuildControl">
+        <div className="Label">{props.label}</div>
+        <button className="Less">Remove</button>
+        <button className="More">Add</button>
+    </div> 
+)
+
+export default buildControl;
