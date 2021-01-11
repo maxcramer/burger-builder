5640ce47fbe505c39576dee83d11820b16b168bc
maxcramer <maximiliancramer@yahoo.com>
2021-01-11 17:10:39 +0000

    adjusting back to where height being passed on logo as prop

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,8 +5,8 @@ import './Logo.css';
 import BurgerLogo from '../../assets/images/burger-logo.png';
 
 const logo = (props) => (
-    <div className="Logo">
-        <img src={BurgerLogo} alt="burger logo" style={{height: props.height}} />
+    <div className="Logo" style={{height: props.height}} >
+        <img src={BurgerLogo} alt="burger logo" />
     </div>
 );
 
