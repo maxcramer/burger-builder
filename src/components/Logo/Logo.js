7f883f894532f84a394f05e8df504e01884fab60
maxcramer <maximiliancramer@yahoo.com>
2021-01-11 16:16:14 +0000

    adding styling to logo and importing to toolbar

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Logo.css';
+
+import BurgerLogo from '../../assets/images/burger-logo.png';
+
+const logo = (props) => (
+    <div className="Logo">
+        <img src={BurgerLogo} alt="burger logo" />
+    </div>
+);
+
+export default logo;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Logo from '../../Logo/Logo';
+
 import './Toolbar.css';
 
 const toolbar = (props) => (
     <header className="Toolbar">
         <div>Menu</div>
-        <div>Logo</div>
+        <Logo />
         <nav>
            ...
         </nav>
