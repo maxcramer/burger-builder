6f23a9685965ca32fc1c6ce10a11705d4fd0a94a
maxcramer <maximiliancramer@yahoo.com>
2021-01-11 13:00:04 +0000

    ingredients being passed as prop to order summary, rendering list of ingredients in Modal dynamically

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Aux'
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return <li>
+                    <span style={{ textTransform: 'capitalize' }}>
+                        {igKey}
+                    </span>
+                    : {props.ingredients[igKey]}
+                </li>
+        })
+
+
+    return (
+        <Aux>
+            <h3>Order Summary</h3>
+            <p>A delicious burger with:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout</p>
+        </Aux>
+    )
+};
+
+export default orderSummary
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-
 import './Modal.css';
 
-const modal = (props) => {
-    <div>{props.children}</div>
-}
+const modal = (props) => (
+    <div className="Modal">{props.children}</div>
+)
 
 export default modal;
