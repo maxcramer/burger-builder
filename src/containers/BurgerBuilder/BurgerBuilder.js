8b645e125ea91eee29db6befb8572eb960e0d055
maxcramer <maximiliancramer@yahoo.com>
2021-01-13 13:45:43 +0000

    error message showing if order doesn't send to database, closing error message on backdrop click

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 
 
@@ -101,7 +102,7 @@ class BurgerBuilder extends Component{
             },
             deliveryMethod: 'Fastest'
         }
-        axios.post('/orders.json', order)
+        axios.post('/order', order)
             .then(response => {
                 this.setState({loading: false, ordering: false})
             })
@@ -147,5 +148,5 @@ class BurgerBuilder extends Component{
 
 };
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
 
