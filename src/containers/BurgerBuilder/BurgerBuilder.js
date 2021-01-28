3a135b78546f4384934c3974157db364891b4f6e
maxcramer <maximiliancramer@yahoo.com>
2021-01-28 17:31:50 +0000

    creating loop in burger builder to check what ingredients we will be recieving

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -117,7 +117,15 @@ class BurgerBuilder extends Component{
         //     .catch(err => {
         //         this.setState({loading: false, ordering: false})
         //     })
-        this.props.history.push('/checkout');
+        const queryParams = [];
+        for(let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: ''
+        });
     }
 
     render() {
