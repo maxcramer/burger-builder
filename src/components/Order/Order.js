d0e5a029987cc9943ebff55808c1fcd1fe2e41db
maxcramer <maximiliancramer@yahoo.com>
2021-01-29 13:20:11 +0000

    adding basic styling to order ingredients

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -11,7 +11,15 @@ const order = (props) => {
     }
 
     const ingredientOutput = ingredients.map(ig => {
-        return <span key={ig.name}>{ig.name} ({ig.amount})</span>
+        return <span 
+                    style={{
+                        textTransform:'capitalize',
+                        display: 'inline-block',
+                        margin: '0 8px',
+                        border: '1px solid grey',
+                        padding: '5px'
+                    }}
+                    key={ig.name}>{ig.name} ({ig.amount})</span>
     })
 
     return (
