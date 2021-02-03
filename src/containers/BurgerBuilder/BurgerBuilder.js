8290a684537ebfaeb4426dd4612d8efb53055ff4
maxcramer <maximiliancramer@yahoo.com>
2021-02-03 16:57:23 +0000

    setting up mapStateToProps and mapDispatchToProps in burgerbuilder.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from '../../axios-orders';
+import { connect } from 'react-redux';
 
 import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
@@ -8,7 +9,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import { resolvePlugin } from '@babel/core';
+import * as actionTypes from '../../store/actions';
 
 
 
@@ -33,6 +34,7 @@ class BurgerBuilder extends Component{
     }
 
     componentDidMount () {
+
         // axios.get('https://react-burger-bar-ed94a-default-rtdb.firebaseio.com/ingredients.json')
         //     .then(response => {
         //         console.log('RESPONSE', response.data)
@@ -161,6 +163,19 @@ class BurgerBuilder extends Component{
 
 
 };
+
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
+        onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName})
+    }
+}
 
 export default withErrorHandler(BurgerBuilder, axios);
 
