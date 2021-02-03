0a939127e6cdf1f74a68409883b4db47eb38d799
maxcramer <maximiliancramer@yahoo.com>
2021-02-03 17:11:28 +0000

    passing totalPrice in reducer to have acces for it and passing ingredients price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,15 +11,6 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actionTypes from '../../store/actions';
 
-
-
-const INGREDIENT_PRICES = {
-    salad: 0.5,
-    cheese: 0.4,
-    meat: 1.3, 
-    bacon: 0.7,
-}
-
 class BurgerBuilder extends Component{
     // constructor(props) {
     //     super();
