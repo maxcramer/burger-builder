82043ad0e00b1611857235182ea3476dce23ae9f
maxcramer <maximiliancramer@yahoo.com>
2021-02-03 16:34:28 +0000

    setting up add and remove ingredient in actions, created reducer func, and using provider in index, moving application to use redux

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
 ReactDOM.render(
   <React.StrictMode>
