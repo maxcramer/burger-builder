586d90aac107670bea88b513cf95b44d8a6f0946
maxcramer <maximiliancramer@yahoo.com>
2021-02-03 17:25:35 +0000

    removing uneeded code from checkout container and passing mapSateToProps in checkout container

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -46,20 +46,8 @@ class BurgerBuilder extends Component{
     }
 
     orderContinueHandler = () => {
-        // alert('You continue!')
-  
-        const queryParams = [];
-        for(let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.state.totalPrice);
-
-        const queryStirng = queryParams.join('&')
-
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryStirng
-        });
+        // alert('You continue!'
+        this.props.history.push('/checkout');
     }
 
     render() {
