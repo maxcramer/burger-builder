701142fa0e67340cc757567c0e000f405dbd405c
maxcramer <maximiliancramer@yahoo.com>
2021-02-04 16:55:07 +0000

    passing actions in burger builder to dispatch

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { createStore } from 'redux'
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import reducer from './store/reducers/reducer';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
   <Provider store={store}>
