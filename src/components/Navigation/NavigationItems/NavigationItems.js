9acf68fa7b8102fbc8af67665b0df065b382d182
maxcramer <maximiliancramer@yahoo.com>
2021-02-15 15:51:27 +0000

    only showing order menu item if authenticated

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -10,9 +10,7 @@ const navigationItems = (props) => (
         <NavigationItem exact link="/" >
             Burger Builder
         </NavigationItem>
-        <NavigationItem link="/orders">
-            Orders
-        </NavigationItem>
+        {props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
         { !props.isAuthenticated 
             ? <NavigationItem link="/auth">Authenticate</NavigationItem> 
             : <NavigationItem link="/logout" >Logout</NavigationItem>
