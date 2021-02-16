b375b489068dfa69b118cfbf742d52d6f6b194cb
maxcramer <maximiliancramer@yahoo.com>
2021-02-16 17:12:50 +0000

    using enviromental variables

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
