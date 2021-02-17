8b3ce31931e380249774f8eb8527eb943b73e5a6
maxcramer <maximiliancramer@yahoo.com>
2021-02-17 14:07:30 +0000

    updating auth.test.js:

--- src/store/reducers/auth.test.js
+++ src/store/reducers/auth.test.js
@@ -9,6 +9,26 @@ describe('auth reducer', () => {
             error: null,
             loading: false,
             authRedirectPath: '/'
+        });
+    });
+
+    it('should store a token on login', () => {
+        expect(reducer({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        }, {
+            type: actionTypes.AUTH_SUCCESS, 
+            idToken: 'some-token',
+            userId: 'some-user-id'
+        })).toEqual({
+            token: 'some-token',
+            userId: 'some-user-id',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
         })
-    })
-})
+    });
+});
