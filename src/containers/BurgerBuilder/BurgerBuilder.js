5e864f3a09e47403ef7c3c5739fc6eba4dd4e046
maxcramer <maximiliancramer@yahoo.com>
2021-03-03 17:25:14 +0000

    reverting burgerbuilder as tutorial was showing very very bad practice so not doing that way, also doesn't even work

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import axios from '../../axios-orders';
 
 import Aux from '../../hoc/Aux/Aux';
